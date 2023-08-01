78c85acaf3433c4b39c5baa397ffa63e2881f7f9
unknown <D:\[internet]\mailbat>
2023-08-01 12:17:45 +0500

    minor tweaks

--- test_product_page.py
+++ test_product_page.py
@@ -48,13 +48,24 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 @pytest.mark.need_review
-def test_guest_can_add_product_to_basket(browser):
-     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-     page = ProductPage(browser, link)
-     page.open()
-     page.click_add_to_card()
-     page.product_name_is_correct()
-     page.product_price_is_correct()
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+                                               "?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_add_to_card()
+    page.solve_quiz_and_get_code()
+    page.product_name_is_correct()
+    page.product_price_is_correct()
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
